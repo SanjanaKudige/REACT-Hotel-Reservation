7aa130d811b3a8b2707abafd5488126c538b8e5f
Sanjana Kudige Anish Kumar <sanju.ka93@gmail.com>
2019-08-20 16:45:26 -0700

    Completed the App.js page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+
+import Home from "./pages/Home"
+import Rooms from "./pages/Rooms"
+import SingleRoom from "./pages/SingleRoom"
+import Error from "./pages/Error"
+
+import { Route, Switch } from "react-router-dom"
+
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <> Hello Sanjana </>
+
+    <>
+      <Navbar />
+      <Switch>
+        <Route ecact path="/" component={Home} />
+        <Route ecact path="/rooms/" component={Rooms} />
+        <Route ecact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
