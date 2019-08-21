4d163f9a94e6e1e7268f931af37cf240bce47aed
Sanjana Kudige Anish Kumar <sanju.ka93@gmail.com>
2019-08-20 17:34:17 -0700

    Designed the banner for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { homedir } from 'os';
 
-export default function Home() {
+
+const home = () => {
     return (
-        <div>
-            Hello from home page
-        </div>
-    )
-}
+        <>
+            <Hero>
+                <Banner
+                    title="Luxurious rooms"
+                    subtitle="deluxe rooms starting at $299"
+                >
+                    <Link to="/rooms" className="btn-primary">
+                        Our rooms
+                    </Link>
+                </Banner>
+            </Hero>
+            {/* <Services />
+            <FeaturedRooms /> */}
+        </>
+    );
+};
+
+export default home;
